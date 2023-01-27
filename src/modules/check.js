515905b4a40d1d856bc118cb00574af35f747ec1
alicemirigo92 <alicemirigo92@gmai.com>
2023-01-27 22:18:50 +0300

    Add changes

--- src/modules/check.js
+++ src/modules/check.js
@@ -1,14 +1,14 @@
-export default class Check {
-  constructor(id) {
-    this.id = id;
-    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-  }
+// export default class Check {
+//   constructor(id) {
+//     this.id = id;
+//     this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+//   }
 
-  checkItem() {
-    const checked = this.todos.find((t) => t.index === +this.id);
-    if (checked) {
-      checked.completed = !checked.completed;
-    }
-    localStorage.setItem('todos', JSON.stringify(this.todos));
-  }
-}
+//   checkItem() {
+//     const checked = this.todos.find((t) => t.index === +this.id);
+//     if (checked) {
+//       checked.completed = !checked.completed;
+//     }
+//     localStorage.setItem('todos', JSON.stringify(this.todos));
+//   }
+// }
--- src/modules/clear.js
+++ src/modules/clear.js
@@ -1,10 +1,10 @@
-export default class Clear {
-  constructor() {
-    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-  }
+// export default class Clear {
+//   constructor() {
+//     this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+//   }
 
-  clearTodos() {
-    const filtered = this.todos.filter((t) => !t.completed);
-    localStorage.setItem('todos', JSON.stringify(filtered));
-  }
-}
+//   clearTodos() {
+//     const filtered = this.todos.filter((t) => !t.completed);
+//     localStorage.setItem('todos', JSON.stringify(filtered));
+//   }
+// }
