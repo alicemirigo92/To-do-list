1a448c020b8aa16b777dedd29f936c4c2e7e4eda
alicemirigo92 <alicemirigo92@gmai.com>
2023-02-02 16:22:11 +0300

    Test edit and check

---
+++ _tests_/check.test.js
@@ -0,0 +1,21 @@
+import Check from '../src/modules/check.js';
+
+// const todos = [
+//   {
+//     description: 'wash',
+//     completed: false,
+//     index: 1,
+//   },
+// ];
+
+localStorage.setItem('todos', JSON.stringify(todos));
+
+describe('The function', () => {
+  test('Updates the selected item`s completed status to true in the localstorage', () => {
+    const check = new Check(1);
+    check.checkItem();
+
+    const todoList = JSON.parse(localStorage.getItem('todos'));
+    expect(todoList[0].completed).toBe(true);
+  });
+})
